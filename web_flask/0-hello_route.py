1048cf33bc2750726eda18d571733973186a976e
J-rgb-ai <opiyojoyshaney@gmail.com>
2024-05-17 16:06:55 +0000

    hi

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_route():
     """
     Route for the root path that displays "Hello HBNB!"
     """
